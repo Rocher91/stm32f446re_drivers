dd62fd9ec50390dd46fb023df550b42d63a0dc2e
Xavier Rocher <xavirbn@gmail.com>
2024-05-07 18:09:13 +0200

    Update gitIgnore

--- Objects/stm32f446xx_adc_driver.d
+++ Objects/stm32f446xx_adc_driver.d
@@ -1,5 +1,5 @@
 ./objects/stm32f446xx_adc_driver.o: Drivers\src\stm32f446xx_ADC_Driver.c \
   Drivers\inc\stm32f446xx_ADC_Driver.h Drivers\inc\stm32f446xx.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h \
   Drivers\inc\stm32f446xx_Gpio_Driver.h
--- Objects/stm32f446xx_gpio_driver.d
+++ Objects/stm32f446xx_gpio_driver.d
@@ -1,5 +1,5 @@
 ./objects/stm32f446xx_gpio_driver.o: \
   Drivers\src\stm32f446xx_Gpio_Driver.c \
   Drivers\inc\stm32f446xx_Gpio_Driver.h Drivers\inc\stm32f446xx.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
+  C:\Keil_v5\ARM\ARMCLANG\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h
